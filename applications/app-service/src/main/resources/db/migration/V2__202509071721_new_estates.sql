1d91e0b6f3b587bc69917c1fa12e9d7bd803a3da
Luis Plata <www.luisplata@gmail.com>
2025-09-18 11:39:18 -0500

    fix name of state

--- applications/app-service/src/main/resources/db/migration/V2__202509071721_new_estates.sql
+++ applications/app-service/src/main/resources/db/migration/V2__202509071721_new_estates.sql
@@ -1,4 +1,4 @@
 INSERT INTO estados (nombre, descripcion)
 VALUES
-('Aprovado', 'La solicitud ha sido aprobada tras la revisión y cumple con todos los requisitos necesarios'),
+('Aprobado', 'La solicitud ha sido aprobada tras la revisión y cumple con todos los requisitos necesarios'),
 ('Rechazado', 'La solicitud ha sido revisada y no cumple con los requisitos necesarios para su aprobación');
